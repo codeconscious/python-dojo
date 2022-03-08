2166fb807f8d73618d75477527508d2e9b6c3050
CodeConscious <hello@codeconscio.us>
2022-03-08 21:34:14 +0900

    Create collections.py

---
+++ collections.py
@@ -0,0 +1,16 @@
+class XPerson:
+    def __init__(self, heroName, normalName, powers):
+        self.heroName = heroName
+        self.normalName = normalName
+        self.powers = powers
+
+    def describe(self):
+        return "> %s (%s) has the power of %s." % (self.heroName, self.normalName, self.powers)
+
+xmen = {}
+xmen["cyclops"] = XPerson("Cyclops", "Scott Summers", "optic blasts")
+xmen["magneto"] = XPerson("Magneto", "Erik Lehnsherr", "magnetism")
+xmen["jubilee"] = XPerson("Jubilee", "Jubilation Lee", "pyrotechnic energy blasts")
+del(xmen["magneto"]) # Or: xmen.pop("magneto")
+for x, y in xmen.items():
+    print(y.describe())
