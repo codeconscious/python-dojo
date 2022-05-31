5e6c376e873cacd82e108e5e94d3e2f852b1f229
CodeConscious <hello@codeconscio.us>
2022-05-31 21:09:35 +0900

    Add random char generator

--- generators.py
+++ generators.py
@@ -1,12 +1,24 @@
 import random
 
+# Return 7 random numbers
 def lottery():
-    # Returns 6 numbers between 1 and 40
     for i in range(6):
         yield random.randint(1, 40)
-
-    # Returns a 7th random number
     yield random.randint(-20, -1)
 
 for random_number in lottery():
     print("• %d" % (random_number))
+
+
+def char_generator(count):
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for i in range(count):
+        yield letters[random.randint(0, 25)]
+
+# Print 75 letters, separating them into groups of 5 for legibility.
+output = ""
+for index, letter in enumerate(char_generator(75)): # Use enumerate to get the index
+    output += letter
+    if (index % 5 == 4):
+        output += " "
+print(output)
